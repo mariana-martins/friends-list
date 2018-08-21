8a36543607d8becb1ccf0caafe635a4ba69b1638
mariana-martins <mariana_martins@id.uff.br>
2018-08-22 09:50:54 +1200

    Adding an input

--- src/index.js
+++ src/index.js
@@ -8,12 +8,22 @@ class HelloWorld extends React.Component{
         this.state = {
             username: "Carlinhos"
         }
+
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({
+            username: e.target.value
+        })
     }
 
     render() {
         return (
             <div>
                 <h1>Hello {this.state.username}</h1>
+                <p>Change name:</p>
+                <input type="text" value={this.state.username} onChange={this.handleChange} />
             </div>
         )
     }
