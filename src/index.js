7d6df127833cd09e291647b88a85c3a70a0df044
mariana-martins <mariana_martins@id.uff.br>
2018-08-22 09:45:49 +1200

    Adding state

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class HelloWorld extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: "Carlinhos"
+        }
+    }
+
     render() {
         return (
             <div>
-                <h1>Hello World!</h1>
+                <h1>Hello {this.state.username}</h1>
             </div>
         )
     }
