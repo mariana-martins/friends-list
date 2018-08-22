e015b9aecf4e0dd5888d9c863c2d6cda2f7e0841
mariana-martins <mariana_martins@id.uff.br>
2018-08-22 15:09:31 +1200

    Input new friends and fixing key problem

--- src/index.js
+++ src/index.js
@@ -9,6 +9,14 @@ class FriendsContainer extends React.Component {
       name: "Ricardo",
       friends: ["Maria Pereira", "Samuel Freitas", "Luiza Maciel"]
     };
+
+    this.addFriend = this.addFriend.bind(this);
+  }
+
+  addFriend(friend) {
+    this.setState(state => ({
+      friends: state.friends.concat([friend])
+    }));
   }
 
   render() {
@@ -18,6 +26,7 @@ class FriendsContainer extends React.Component {
           Name:
           {this.state.name}
         </h3>
+        <AddFriend addNew={this.addFriend} />
         <ShowList names={this.state.friends} />
       </div>
     );
@@ -26,15 +35,53 @@ class FriendsContainer extends React.Component {
 
 class ShowList extends React.Component {
   render() {
+    const friend = this.props.names;
     return (
       <div>
         <h3>Friends</h3>
         <ul>
-          {" "}
-          {this.props.names.map(friend => (
-            <li>{friend}</li>
+          {friend.map(friend => (
+            <li key={friend.toString()}>{friend}</li>
           ))}
         </ul>
+      </div>
+    );
+  }
+}
+
+class AddFriend extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newFriend: " "
+    };
+
+    this.updateNewFriend = this.updateNewFriend.bind(this);
+    this.handleAddNew = this.handleAddNew.bind(this);
+  }
+
+  updateNewFriend(e) {
+    this.setState({
+      newFriend: e.target.value
+    });
+  }
+
+  handleAddNew() {
+    this.props.addNew(this.state.newFriend);
+    this.setState({
+      newFriend: ""
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.newFriend}
+          onChange={this.updateNewFriend}
+        />
+        <button onClick={this.handleAddNew}>Add New Friend</button>
       </div>
     );
   }
